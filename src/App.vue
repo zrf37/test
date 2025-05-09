cbe57c97d4d5a7a0874cf358097d9d054de24063
zrf37 <1976504347@qq.com>
2025-05-09 16:08:13 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello Azure + Vue 3!</h1>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  root: 'frontend',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+  plugins: [vue()]
 })
